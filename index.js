3648c6e51f9c7f087133c29758c175ff60b259af
Bharat Nagda <nagdabharat@gmail.com>
2020-06-07 08:20:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ function preserveDecimal(string)  {
 function add(numberOne, numberTwo) {
   return(numberOne + numberTwo)
 }
+
+function subtract(numberOne, numberTwo) {
+  return(numberOne - numberTwo)
+}
+
+function divide(numberOne, numberTwo) {
+  return(numberOne / numberTwo)
+}
+
+function multiply(numberOne, numberTwo) {
+  return(numberOne * numberTwo)
+}
